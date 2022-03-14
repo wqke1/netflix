ff2c0565dcb5ca1fa8236cc701afd03ac095c017
Theo Faugere <theo.faugere3@gmail.com>
2022-03-14 18:02:19 +0100

    salut

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get ‘categories/index’
+  get ‘categories/show’
+  get ‘movies/index’
+  get ‘movies/show’
+
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
