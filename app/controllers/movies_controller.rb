f16c604a0f5200ff72109b666fc4f5fbc9a107c4
Theo Faugere <theo.faugere3@gmail.com>
2022-03-15 19:12:38 +0100

    parse c morts

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,22 @@
+require "open-uri"
+require "json"
+
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    @movie = scrap
   end
 
   def show
-    @movies = Movie.find(params[id])
+    @movies = Movie.find(params[:id])
+  end
+
+  private
+
+  ## Scraping
+
+  def scrap(index)
+    url = "https://api.themoviedb.org/3/movie/#{index}?api_key=444db8b8fcf0f7b3af4b52ce1dfd38b8"
+    movie_serialized = URI.open(url).read
+    movie = JSON.parse(movie_serialized)
   end
 end
