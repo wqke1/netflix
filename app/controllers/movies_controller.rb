975c39f6d375aacb69d4b11e4cb45aa856c4c503
Thomas Loubet <thommasloubet@hotmail.fr>
2022-03-17 18:34:39 +0100

    cadeau theo

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,15 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    @movies = Movie.all.sample(6)
+    @movies_letop10 = Movie.all.sample(6)
+    @movies_tendances = Movie.all.sample(6)
+    @movies_new = Movie.all.sample(6)
+    @movies_chill = Movie.all.sample(6)
+    @movies_populaire = Movie.all.sample(6)
+    @movies_hollywood = Movie.all.sample(6)
+    @movies_comedies = Movie.all.sample(6)
+    @movies_anime = Movie.all.sample(6)
+    @movies_sports = Movie.all.sample(6)
   end
 
   def show
